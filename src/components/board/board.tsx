fed6a3640ef9b2b5febed1b5420a59fb026ee20d
joakimnagornoff <joakim.nagornoff@live.se>
2025-02-04 18:14:31 +0100

    feat: implemented logic for isValidMove and handleTileClick

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -1,36 +1,65 @@
 import React, { useEffect, useState } from 'react'
 import './board.css'
-import PuzzleItem from '../puzzleItem/puzzleItem.tsx'
+import { PuzzleItem, PuzzleItemEmpty } from '../puzzleItem/puzzleItem.tsx'
+
 
 const Board = () => {
-  const [positions, setPositions] = useState([...Array(15).keys()])
+  const [positions, setPositions] = useState<(number | null)[]>(() => {
+    const numbers = [...Array(14).keys()].map(n => n + 1);
+    return [...numbers, null] as (number | null)[];
+  });
+
+  const emptyIndex = positions.indexOf(null);
+  const rowSize = 5;
+
 
   const shuffle = () => {
     setPositions((prevPositions) => {
-      const shuffledPositions = [...prevPositions]
+      const shuffledPositions = [...prevPositions];
       shuffledPositions.sort(() => Math.random() - 0.5);
-      return shuffledPositions
+      return shuffledPositions;
     })
   }
 
 
+  const isValidMove = (clickedIndex: number) => {
+    return (
+      (clickedIndex === emptyIndex - 1 && emptyIndex % rowSize !== 0) || // Left
+      (clickedIndex === emptyIndex + 1 && clickedIndex % rowSize !== 0) || // Right
+      clickedIndex === emptyIndex - rowSize || // Above
+      clickedIndex === emptyIndex + rowSize // Below
+    );
+  }
+
+  const handleTileClick = (index: number) => {
+    if (!isValidMove(index)) return
+
+    const newPositions = [...positions];
+
+    [newPositions[index], newPositions[emptyIndex]] = [newPositions[emptyIndex], newPositions[index]];
+
+    setPositions(newPositions);
+  }
+
   useEffect(() => {
     setPositions((prevPositions) => {
       const newPos = [...prevPositions];
-      newPos.sort((a, b) => a > b ? 1 : -1);
-      return newPos
+      newPos.sort((a, b) => (a !== null && b !== null ? a - b : a === null ? 1 : -1));
+      return newPos;
     })
   }, [])
+
   return (
     <>
       <div className='board-container'>
         {positions.map((pos, index) => {
-          const number = pos === 14 ? null : pos + 1;
-          return (
-            number ?
-              <PuzzleItem key={index} number={number} onClick={() => console.log(`cliked ${number}`)} />
-              : null
-          )
+          const number = pos === null ? null : pos;
+
+          return number !== null ? (
+            <PuzzleItem key={index} index={index} number={number} onClick={() => handleTileClick(index)} />
+          ) : (
+            <PuzzleItemEmpty key={index} index={index} onClick={() => handleTileClick(index)} />
+          );
         })}
       </div>
       <div className='button-container'>
